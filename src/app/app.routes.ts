1b1eda3122a58e095b13abc4d5ef6c1d1d4cfaf4
maajoopaa <dhdhsjgsh@gmail.com>
2025-11-16 22:26:42 +0300

    Added route for profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {PostComponent} from './sharedComponents/post-component/post-component';
 import {PostsComponent} from './sharedComponents/posts-component/posts-component';
 import {ChatComponent} from './sharedComponents/chat-component/chat-component';
 import {ChatsComponent} from './sharedComponents/chats-component/chats-component';
+import {ProfileComponent} from './sharedComponents/profile-component/profile-component';
 
 export const routes: Routes = [
   {path: "", component: MainLayoutComponent,
   children: [
     {path: "home", component: PostsComponent},
     {path: "chats", component: ChatsComponent},
+    {path: "profile",component: ProfileComponent},
   ]},
 ];
